ab9ad250b4600d1db01263afbdbdf2b702589a2b
Tuheen <tuheen.ojha@gmail.com>
2017-06-17 15:05:25 -0700

    push

---
+++ src/components/Resources.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Resources extends Component {
+
+    render() {
+        return (<div>
+            <ul>Secret Heroes
+                <li>JsonStratmu</li>
+                <li>Heros</li>
+                <li>Lance</li>
+            </ul>
+        </div>);
+    }
+}
